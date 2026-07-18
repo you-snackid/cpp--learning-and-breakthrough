dc43bfb44925af614a08c8c2aaf09aca693676c0
you-snackid <wzry76890@outlook.com>
2026-07-19 07:02:24 +0800

    string

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string firstName = "张";
+    string lastName = "三";
+
+    string fullName = firstName + " " + lastName;
+    cout << fullName << endl; 
+
+    string str = "Hello";
+    str += " World";
+    str += "!";
+    cout << str << endl;
+    return 0;
+}
